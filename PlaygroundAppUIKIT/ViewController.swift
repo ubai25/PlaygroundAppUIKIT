6488f0fb572199d7fc28886f2997799cead642a9
ubai <a.ubai25@gmail.com>
2024-12-07 18:59:44 +0700

    initial commit

---
+++ PlaygroundAppUIKIT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlaygroundAppUIKIT
+//
+//  Created by Ubaidillah Ahmad on 07/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
